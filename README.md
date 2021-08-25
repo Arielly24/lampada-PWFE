ca6f7703c25f54451b19517ebd1f1a0e9ade9be1
ARIELLY DA SILVA ANGELO <21192993@CFP127ALUNO.LOCAL>
2021-08-25 11:12:41 -0300

    finalizado quebrar lampada e readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# lampada-PWFE
+# Lâmpada-PWFE
+
+Projeto criado com o objetivo didático para as aulas de PWFE do curso de Desenvolvimento de Sistemas do [Senai Jandira](https://jandira.sp.senai.br/), sob orientação do professor [Fernando Leonid](https://github.com/FernandoLeonid). 
+
+O projeto consiste em criar uam lâmpada que tenha três ações:
+*ligar 
+*desligar
+*quebrar
+Todo o código foi construido seguindo as boas práticas, como responsabilidade única e funções puras.
+![](img/lampada.png) 
+
+
--- lampada.js
+++ lampada.js
@@ -1,17 +1,66 @@
 "use strict"
-function ligarLampada (){
+
+
+
+function lampadaQuebrada(){
     const lampada = document.getElementById("lampada")
-    lampada.src = "img/ligada.jpg"
+    return lampada.src.includes("quebrada") 
 }
 
-document.getElementById("ligar")
-.addEventListener("click", ligarLampada)
+function quebrarLampada() {
+    const lampada = document.getElementById("lampada")
+    const botaoLigar = document.getElementById("ligar")
+    const botaoDesligar = document.getElementById("desligar")
+
+    lampada.src = "img/quebrada.jpg"
+    botaoLigar.disable = false
+    botaoDesligar.disable = true    
+}
+
+
+
+function ligarLampada (){
+    const lampada = document.getElementById("lampada")
+    const botaoLigar = document.getElementById("ligar")
+    const botaoDesligar = document.getElementById("desligar")
+
+    if (!lampadaQuebrada()){
+        lampada.src = "img/ligada.jpg"
+        botaoLigar.disable = true
+        botaoDesligar.disable = false
+    }
+
+
+}
+
 
 function desligarLampada (){
     const lampada = document.getElementById("lampada")
+    const botaoLigar = document.getElementById("ligar")
+    const botaoDesligar = document.getElementById("desligar")
+
     lampada.src = "img/desligada.jpg"
+    botaoLigar.disable = false
+    botaoDesligar.disable = true
 }
 
 document.getElementById("desligar")
 .addEventListener("click", desligarLampada)
+
+
+
+document.getElementById("ligar")
+.addEventListener("click", ligarLampada)
+
+
+document.getElementById("lampada")
+.addEventListener("mouseover", ligarLampada)
+
+document.getElementById("lampada")
+.addEventListener("mouseleave", desligarLampada)
+
+document.getElementById("lampada")
+.addEventListener("dblclick", quebrarLampada)
+
+
 
